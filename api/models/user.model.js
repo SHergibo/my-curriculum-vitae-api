548747b32b56315cb2f43708520e3c3217183206
SHergibo <sachau888@hotmail.com>
2021-06-30 16:14:41 +0200

    feat: Added unique and match schema valitation for email field to check if email is valid and unique in user.model.

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -4,140 +4,142 @@ const Mongoose = require('mongoose'),
       Bcrypt = require('bcrypt'),
       Boom = require('@hapi/boom');
 
-const {env, jwtSecret, jwtExpirationInterval} = require('../../config/environment.config');
+const { env, jwtSecret, jwtExpirationInterval } = require('../../config/environment.config');
 
 const roles = ['admin', 'user', 'ghost'];
 
 let Schema = Mongoose.Schema;
 
 let schema = new Schema({
-    username : {
-        type : String,
-        unique : true,
-        trim : true
-    },
-    firstname : {
-        type : String,
-        required : true,
-        trim : true
-    },
-    lastname : {
-        type : String,
-        required : true,
-        trim : true
-    },
-    email : {
-        type : String,
-        required : 'You must specify an email',
-        trim : true,
-        lowercase : true
-    },
-    password : {
-        type : String,
-        required : true,
-        minlength : 6,
-        maxlength : 128
-    },
-    role : {
-        type : String,
-        enum : roles,
-        required : true
-    },
+  username: {
+    type: String,
+    unique: true,
+    trim: true
+  },
+  firstname: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  lastname: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: 'You must specify an email',
+    trim: true,
+    lowercase: true,
+    unique: true,
+    match: [/^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()\.,;\s@\"]+\.{0,1})+([^<>()\.,;:\s@\"]{2,}|[\d\.]+))$/, 'Please fill a valid email address']
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+    maxlength: 128
+  },
+  role: {
+    type: String,
+    enum: roles,
+    required: true
+  },
 });
 
-schema.pre('save', async function(next){
-    try {
-        if(!this.isModified('password')){
-            return next();
-        }
-        let salt = env === 'staging' ? 1 : 10;
-        let hash = await Bcrypt.hash(this.password, salt);
-        this.password = hash;
-        return next();
-    } catch (error) {
-        next(Boom.badImplementation(error.message));
+schema.pre('save', async function (next) {
+  try {
+    if (!this.isModified('password')) {
+      return next();
     }
+    let salt = env === 'staging' ? 1 : 10;
+    let hash = await Bcrypt.hash(this.password, salt);
+    this.password = hash;
+    return next();
+  } catch (error) {
+    next(Boom.badImplementation(error.message));
+  }
 });
 
-schema.methods.token = function(){
-    const payload = {
-        iat : Moment().unix(),
-        exp : Moment().add(jwtExpirationInterval, 'minutes').unix(),
-        sub : this._id
-    };
-    return Jwt.encode(payload, jwtSecret);
+schema.methods.token = function () {
+  const payload = {
+    iat: Moment().unix(),
+    exp: Moment().add(jwtExpirationInterval, 'minutes').unix(),
+    sub: this._id
+  };
+  return Jwt.encode(payload, jwtSecret);
 };
 
-schema.methods.passwordMatches = async function(pwd){
-    return await Bcrypt.compare(pwd, this.password);
+schema.methods.passwordMatches = async function (pwd) {
+  return await Bcrypt.compare(pwd, this.password);
 };
 
-schema.methods.transform = function() {
-    const fields = ['_id', 'username', 'firstname', 'lastname', 'email', 'role'];
-    const object = {};
-    fields.forEach((field)=>{
-        object[field] = this[field];
-    });
-    return object;
+schema.methods.transform = function () {
+  const fields = ['_id', 'username', 'firstname', 'lastname', 'email', 'role'];
+  const object = {};
+  fields.forEach((field) => {
+    object[field] = this[field];
+  });
+  return object;
 };
 
 schema.statics.roles = roles;
 
-schema.statics.get = async function(id) {
-    try {
-        let user;
-        if(!Mongoose.Types.ObjectId.isValid(id)){
-            throw Boom.badRequest('ID is not valid');
-        }
-
-        user = await this.findById(id);
-
-        if(!user){
-            throw Boom.notFound('User not found');
-        }
-        return user;
-    } catch (error) {
-        throw Boom.badImplementation(err.message);
+schema.statics.get = async function (id) {
+  try {
+    let user;
+    if (!Mongoose.Types.ObjectId.isValid(id)) {
+      throw Boom.badRequest('ID is not valid');
     }
+
+    user = await this.findById(id);
+
+    if (!user) {
+      throw Boom.notFound('User not found');
+    }
+    return user;
+  } catch (error) {
+    throw Boom.badImplementation(err.message);
+  }
 };
 
-schema.statics.findAndGenerateToken = async function(options){
-    const { email, password, refreshObject } = options;
+schema.statics.findAndGenerateToken = async function (options) {
+  const { email, password, refreshObject } = options;
 
-    if(!email) throw Boom.badRequest('An email is required to generate a token');
-    if(!refreshObject){
-        if(!password) throw Boom.badRequest('A password is required to authorize a token generation');
+  if (!email) throw Boom.badRequest('An email is required to generate a token');
+  if (!refreshObject) {
+    if (!password) throw Boom.badRequest('A password is required to authorize a token generation');
+  }
+
+  const user = await this.findOne({ email });
+  if (!user) {
+    throw Boom.notFound('User not found');
+  }
+
+  if (!refreshObject) {
+    if (await user.passwordMatches(password) === false) {
+      throw Boom.unauthorized('Password must match to authorize a token generation');
     }
+  }
 
-    const user = await this.findOne({email});
-    if(!user){
-        throw Boom.notFound('User not found');
-    }
+  if (refreshObject && refreshObject.userEmail === email && Moment(refreshObject.expires).isBefore()) {
+    throw Boom.unauthorized('Invalid refresh token');
+  }
 
-    if(!refreshObject){
-        if(await user.passwordMatches(password) === false){
-            throw Boom.unauthorized('Password must match to authorize a token generation');
-        }
-    }
-
-    if(refreshObject && refreshObject.userEmail === email && Moment(refreshObject.expires).isBefore()){
-        throw Boom.unauthorized('Invalid refresh token');
-    }
-
-    return { user, accessToken : user.token() };
+  return { user, accessToken: user.token() };
 };
 
-schema.statics.checkDuplicateEmail = function(error){
-    if(error.name === "MongoError" && error.code === 11000){
-        return Boom.conflict('Validation error', {
-            errors : [{
-                field : "email",
-                location : 'body',
-                messages : ['"Email" already exists']
-            }]
-        });
-    }
-    return error;
+schema.statics.checkDuplicateEmail = function (error) {
+  if (error.name === "MongoError" && error.code === 11000) {
+    return Boom.conflict('Validation error', {
+      errors: [{
+        field: "email",
+        location: 'body',
+        messages: ['"Email" already exists']
+      }]
+    });
+  }
+  return error;
 }
 
 module.exports = Mongoose.model('User', schema);
