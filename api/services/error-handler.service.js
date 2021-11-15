fb529180b89690fe8271c33642467ec4298b5f89
SHergibo <sachau888@hotmail.com>
2021-11-15 14:24:08 +0100

    feat: Added logic to serve an html page or a json object when the app return a 404 in error-handler.service.js.

--- api/services/error-handler.service.js
+++ api/services/error-handler.service.js
@@ -1,37 +1,49 @@
-const {loggerError} = require('../../config/logger.config'),
-      Notifier = require('node-notifier'),
-      { env, environments } = require('./../../config/environment.config');
-      Boom = require('@hapi/boom');
+const { loggerError } = require('../../config/logger.config'),
+  Notifier = require('node-notifier'),
+  { env, environments } = require('./../../config/environment.config');
+Boom = require('@hapi/boom');
 
-exports.log = (err, req, res, next) =>{
-    let error = {"message" : `Error in ${req.method} ${req.url}`, "stack": err.stack};
-    loggerError.error(error);
-    next(err);
+exports.log = (err, req, res, next) => {
+  let error = { "message": `Error in ${req.method} ${req.url}`, "stack": err.stack };
+  loggerError.error(error);
+  next(err);
 };
 
 exports.notify = (err, str, req) => {
-    let title = `Error in ${req.method} ${req.url}`;
-    Notifier.notify({
-        title : title,
-        message : str
-    })
+  let title = `Error in ${req.method} ${req.url}`;
+  Notifier.notify({
+    title: title,
+    message: str
+  })
 };
 
-exports.exit = (err, req, res, next) =>{
-    let code = 500;
-    let errorBoom = err;
-    if (err.boom) errorBoom = err.boom;
-    if (errorBoom.output) code = errorBoom.output.statusCode;
-    if (errorBoom.httpsStatusCode) code = errorBoom.httpsStatusCode;
-    if (env.toUpperCase() === environments.DEVELOPMENT) {
-      if (err.error) console.log(err.error);
-      console.log(errorBoom);
-    }
-    res.status(code);
-    res.json(errorBoom);
+exports.exit = (err, req, res, next) => {
+  let code = 500;
+  let errorBoom = err;
+  if (err.boom) errorBoom = err.boom;
+  if (errorBoom.output) code = errorBoom.output.statusCode;
+  if (errorBoom.httpsStatusCode) code = errorBoom.httpsStatusCode;
+  if (env.toUpperCase() === environments.DEVELOPMENT) {
+    if (err.error) console.log(err.error);
+    console.log(errorBoom);
+  }
+  res.status(code);
+  res.json(errorBoom);
 };
 
 exports.notFound = (req, res, next) => {
-    res.status(404);
-    res.json(Boom.notFound('End point not found'));
+  res.status(404);
+
+  if (req.accepts('text/html')) {
+    res.render('pages/404', {
+      title: '404 - données non trouvées !',
+      layout: 'layouts/default-layout.hbs'
+    })
+    return;
+  }
+
+  if (req.accepts('application/json')) {
+    res.json(Boom.notFound(`End point not found`));
+    return;
+  }
 }
