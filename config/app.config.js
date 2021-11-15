326451bc99802af64a7ac3b986803763251197f6
SHergibo <sachau888@hotmail.com>
2021-11-15 14:23:44 +0100

    feat: Added express-hbs engine and change CORS in app.config.js

--- config/app.config.js
+++ config/app.config.js
@@ -7,17 +7,26 @@ const Express = require('express'),
       Router = require('./../api/routes/v1'),
       Passport = require('passport'),
       Strategies = require('./passport.config'),
-      ServiceErrorHandler = require('../api/services/error-handler.service');
+      ServiceErrorHandler = require('../api/services/error-handler.service'),
+      hbs = require('express-hbs');
 
 const { HTTPLogs, api, env, environments, CorsOrigin } = require('./environment.config');
 
 const app = Express();
 
+app.use('/assets', Express.static(`${process.cwd()}/api/public`));
+
+app.engine('hbs', hbs.express4({
+  defaultLayout : `${process.cwd()}/api/views/layouts/default-layout.hbs`
+}));
+
+app.set('views', `${process.cwd()}/api/views`);
+
+app.set('view engine', 'hbs');
+
 app.use( Helmet() );
 
 app.use( Compression() );
-
-app.use( Express.static('public'));
 
 app.use( Express.json() );
 app.use( Express.urlencoded({extended:true}));
@@ -25,13 +34,7 @@ app.use( Express.urlencoded({extended:true}));
 app.use(mongoSanitize());
 
 app.use(Cors({
-    origin: function (origin, callback) {
-        if (CorsOrigin.indexOf(origin) !== -1) {
-          callback(null, true)
-        } else {
-          callback(new Error('Not allowed by CORS'))
-        }
-      }
+  origin: CorsOrigin
 }));
 
 app.use( Passport.initialize() );
