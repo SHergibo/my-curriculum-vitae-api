aabada96c37507367ffd5deb44bb750e91713b86
SHergibo <sachau888@hotmail.com>
2021-11-15 14:57:37 +0100

    fix: Changed on CORS_ORIGIN in production.env and app.config.js.

--- config/app.config.js
+++ config/app.config.js
@@ -17,27 +17,33 @@ const app = Express();
 app.use('/assets', Express.static(`${process.cwd()}/api/public`));
 
 app.engine('hbs', hbs.express4({
-  defaultLayout : `${process.cwd()}/api/views/layouts/default-layout.hbs`
+  defaultLayout: `${process.cwd()}/api/views/layouts/default-layout.hbs`
 }));
 
 app.set('views', `${process.cwd()}/api/views`);
 
 app.set('view engine', 'hbs');
 
-app.use( Helmet() );
+app.use(Helmet());
 
-app.use( Compression() );
+app.use(Compression());
 
-app.use( Express.json() );
-app.use( Express.urlencoded({extended:true}));
+app.use(Express.json());
+app.use(Express.urlencoded({ extended: true }));
 
 app.use(mongoSanitize());
 
 app.use(Cors({
-  origin: CorsOrigin
+  origin: function (origin, callback) {
+    if (CorsOrigin.indexOf(origin) !== -1 || !origin) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
 }));
 
-app.use( Passport.initialize() );
+app.use(Passport.initialize());
 
 Passport.use('jwt', Strategies.jwt);
 
@@ -45,12 +51,12 @@ app.use(`/api/${api}`, Router);
 
 app.use(Morgan(HTTPLogs));
 
-if(env.toUpperCase() === environments.DEVELOPMENT) {
-    app.use(ServiceErrorHandler.exit);
-    app.use(ServiceErrorHandler.notFound);
-}else{
-    app.use(ServiceErrorHandler.log, ServiceErrorHandler.exit);
-    app.use(ServiceErrorHandler.notFound);
+if (env.toUpperCase() === environments.DEVELOPMENT) {
+  app.use(ServiceErrorHandler.exit);
+  app.use(ServiceErrorHandler.notFound);
+} else {
+  app.use(ServiceErrorHandler.log, ServiceErrorHandler.exit);
+  app.use(ServiceErrorHandler.notFound);
 }
 
 module.exports = app;
