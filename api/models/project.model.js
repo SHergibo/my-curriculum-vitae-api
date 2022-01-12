5c75c881abd5562cc2e83d50adc841422fa0fb78
SHergibo <sachau888@hotmail.com>
2022-01-12 16:39:38 +0100

    feat: Added createdAt timestamp in project model.

--- api/models/project.model.js
+++ api/models/project.model.js
@@ -3,53 +3,55 @@ const Mongoose = require('mongoose');
 let Schema = Mongoose.Schema;
 
 let schema = new Schema({
-    projectName : {
-        type : String,
-        required : true,
-        trim : true
-    },
-    description : {
-        type : String,
-        required : true,
-        trim : true
-    },
-    img : {
-        type : Object,
-        required : true,
-        trim : true
-    },
-    altImg : {
-        type : String,
-        required : true,
-        trim : true
-    },
-    technoUsedFront : {
-        type : Object,
-        required : true,
-    },
-    technoUsedBack : {
-        type : Object,
-        required : true,
-    },
-    url : {
-        type : String,
-        required : true,
-        trim : true
-    },
-    userId : {
-        type : Schema.Types.ObjectId,
-        ref : 'User',
-        required : true,
-    },
+  projectName: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  img: {
+    type: Object,
+    required: true,
+    trim: true
+  },
+  altImg: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  technoUsedFront: {
+    type: Object,
+    required: true,
+  },
+  technoUsedBack: {
+    type: Object,
+    required: true,
+  },
+  url: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  }
+}, {
+  timestamps: { createdAt: true, updatedAt: false }
 });
 
-schema.methods.transformProject = function() {
-    const fields = ['_id', 'projectName', 'description', 'img', "altImg", 'technoUsedFront', 'technoUsedBack', 'url'];
-    const object = {};
-    fields.forEach((field)=>{
-        object[field] = this[field];
-    });
-    return object;
+schema.methods.transformProject = function () {
+  const fields = ['_id', 'projectName', 'description', 'img', "altImg", 'technoUsedFront', 'technoUsedBack', 'url'];
+  const object = {};
+  fields.forEach((field) => {
+    object[field] = this[field];
+  });
+  return object;
 };
 
 module.exports = Mongoose.model('Project', schema);
