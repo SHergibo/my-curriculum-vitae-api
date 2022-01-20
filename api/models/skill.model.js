baea68c912e415173cd0901bf2ae7f1b3b4bd782
SHergibo <sachau888@hotmail.com>
2022-01-20 14:11:27 +0100

    feat: Used isomorphic-dompurify for svgIcon field in skill model.

--- api/models/skill.model.js
+++ api/models/skill.model.js
@@ -1,45 +1,49 @@
-const Mongoose = require('mongoose');
+const Mongoose = require('mongoose'),
+      DomPurify = require('isomorphic-dompurify');
 
 let Schema = Mongoose.Schema;
 
 const SkillCategory = ['generalSkill', 'codingSkill', 'language'];
 
 let schema = new Schema({
-    nameSkill : {
-        type : String,
-        required : true,
-        trim : true
-    },
-    percentage : {
-        type : Number,
-        required : true,
-        trim : true
-    },
-    skillCategory : {
-        type : String,
-        enum : SkillCategory,
-        required : true
-    },
-    fontAwesomeIcon : {
-        type : String,
-    },
-    svgIcon : {
-        type : String,
-    },
-    userId : {
-        type : Schema.Types.ObjectId,
-        ref : 'User',
-        required : true,
-    },
+  nameSkill: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  percentage: {
+    type: Number,
+    required: true,
+    trim: true
+  },
+  skillCategory: {
+    type: String,
+    enum: SkillCategory,
+    required: true
+  },
+  fontAwesomeIcon: {
+    type: String,
+  },
+  svgIcon: {
+    type: String,
+    set: function (val) {
+      return DomPurify.sanitize(val);
+    }
+  },
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
 });
 
-schema.methods.transformSkill = function() {
-    const fields = ['_id', 'nameSkill', 'percentage', 'skillCategory', 'fontAwesomeIcon', 'svgIcon'];
-    const object = {};
-    fields.forEach((field)=>{
-        object[field] = this[field];
-    });
-    return object;
+schema.methods.transformSkill = function () {
+  const fields = ['_id', 'nameSkill', 'percentage', 'skillCategory', 'fontAwesomeIcon', 'svgIcon'];
+  const object = {};
+  fields.forEach((field) => {
+    object[field] = this[field];
+  });
+  return object;
 };
 
 module.exports = Mongoose.model('Skill', schema);
