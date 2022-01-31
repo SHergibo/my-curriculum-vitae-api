e30554d670f53be4a1e3538ae44b199baf111eba
SHergibo <sachau888@hotmail.com>
2022-01-31 16:22:41 +0100

    fix: Deleted /tokens route in index.js.

--- api/routes/v1/index.js
+++ api/routes/v1/index.js
@@ -1,24 +1,23 @@
-const Express = require('express'),
-      UserRoutes = require('./user.route'),
-      MailRoutes = require('./mail.route'),
-      InfoRoutes = require('./info.route'),
-      EducExpeRoutes = require('./educationExperience.route'),
-      SkillRoutes = require('./skill.route'),
-      ProjectRoutes = require('./project.route'),
-      AuthRoutes = require('./auth.route'),
-      TokenAuthRoutes = require('./token-auth.route');
+const Express = require("express"),
+  UserRoutes = require("./user.route"),
+  MailRoutes = require("./mail.route"),
+  InfoRoutes = require("./info.route"),
+  EducExpeRoutes = require("./educationExperience.route"),
+  SkillRoutes = require("./skill.route"),
+  ProjectRoutes = require("./project.route"),
+  AuthRoutes = require("./auth.route"),
+  TokenAuthRoutes = require("./token-auth.route");
 
 const router = Express.Router();
 
-router.get('/status', (req, res) => res.send(200));
+router.get("/status", (req, res) => res.send(200));
 
-router.use('/auth', AuthRoutes);
-router.use('/users', UserRoutes);
-router.use('/mail', MailRoutes);
-router.use('/info', InfoRoutes);
-router.use('/educExpe', EducExpeRoutes);
-router.use('/skill', SkillRoutes);
-router.use('/project', ProjectRoutes);
-router.use('/tokens', TokenAuthRoutes);
+router.use("/auth", AuthRoutes);
+router.use("/users", UserRoutes);
+router.use("/mail", MailRoutes);
+router.use("/info", InfoRoutes);
+router.use("/educExpe", EducExpeRoutes);
+router.use("/skill", SkillRoutes);
+router.use("/project", ProjectRoutes);
 
 module.exports = router;
