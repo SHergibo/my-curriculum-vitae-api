52c01259f1c423503b799756059c547e7a80ecc9
SHergibo <sachau888@hotmail.com>
2022-01-31 16:21:09 +0100

    fix: Deleted register auth controller and route.

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,87 +1,63 @@
-const HttpStatus = require('http-status'),
-      User = require('./../models/user.model'),
-      RefreshToken = require('./../models/refresh-token.model'),
-      Moment = require('moment-timezone');
+const HttpStatus = require("http-status"),
+  User = require("./../models/user.model"),
+  RefreshToken = require("./../models/refresh-token.model"),
+  Moment = require("moment-timezone");
 
-const { jwtExpirationInterval } = require('./../../config/environment.config');
-
+const { jwtExpirationInterval } = require("./../../config/environment.config");
 
 /**
-* Build a token response and return it
-*
-* @param {Object} user
-* @param {String} accessToken
-*
-* @returns A formated object with tokens
-*
-* @private
-*/
+ * Build a token response and return it
+ *
+ * @param {Object} user
+ * @param {String} accessToken
+ *
+ * @returns A formated object with tokens
+ *
+ * @private
+ */
 
 const _generateTokenResponse = function (user, accessToken) {
   const tokenType = "Bearer";
   const refreshToken = RefreshToken.generate(user);
-  const expiresIn = Moment().add(jwtExpirationInterval, 'minutes');
+  const expiresIn = Moment().add(jwtExpirationInterval, "minutes");
   return { tokenType, accessToken, refreshToken, expiresIn };
 };
 
 /**
- * Create and save a new user
+ * Connect user if valid username and password are provided
  *
  * @param {Object} req
  * @param {Object} res
  * @param {Function} next
- * 
+ *
  * @return JWT|next
- * 
- * @public
- */
-exports.register = async (req, res, next) => {
-  try {
-    const user = await (new User(req.body)).save();
-    const token = _generateTokenResponse(user, user.token());
-    res.status(HttpStatus.CREATED);
-    return res.json({ token, user: user.transform() });
-  } catch (error) {
-    return next(User.checkDuplicateEmail(err));
-  }
-};
-
-/**
- * Connect user if valid username and password are provided
- * 
- * @param {Object} req
- * @param {Object} res
- * @param {Function} next
- * 
- * @return JWT|next
- * 
+ *
  * @public
  */
 exports.login = async (req, res, next) => {
   try {
-    const checkRole = await User.findOne({email : req.body.email});
-    if(checkRole.role !== "ghost"){
+    const checkRole = await User.findOne({ email: req.body.email });
+    if (checkRole.role !== "ghost") {
       const { user, accessToken } = await User.findAndGenerateToken(req.body);
       const token = _generateTokenResponse(user, accessToken);
       return res.json({ token, user: user.transform() });
-    }else{
-      return next(Boom.forbidden('Please, verify your account first'));
+    } else {
+      return next(Boom.forbidden("Please, verify your account first"));
     }
-    
   } catch (error) {
     return next(error);
   }
 };
 
 /**
- * Refresh JWT token by RefreshToken removing, and re-creating 
- * 
+ * Refresh JWT token by RefreshToken removing, and re-creating
+ *
  * @param {Object} req
  * @param {Object} res
  * @param {Function} next
- * 
+ *
  * @return JWT|next
- * 
+ *
  * @public
  */
 exports.refresh = async (req, res, next) => {
@@ -89,9 +65,12 @@ exports.refresh = async (req, res, next) => {
     const { email, refreshToken } = req.body;
     const refreshObject = await RefreshToken.findOneAndDelete({
       userEmail: email,
-      token: refreshToken
+      token: refreshToken,
     });
-    const { user, accessToken } = await User.findAndGenerateToken({ email, refreshObject });
+    const { user, accessToken } = await User.findAndGenerateToken({
+      email,
+      refreshObject,
+    });
     const response = _generateTokenResponse(user, accessToken);
     return res.json(response);
   } catch (error) {
@@ -101,25 +80,28 @@ exports.refresh = async (req, res, next) => {
 
 /**
  * logout user and delete token
- * 
+ *
  * @param {Object} req
  * @param {Object} res
  * @param {Function} next
- * 
+ *
  * @return JWT|next
- * 
+ *
  * @public
  */
-exports.logout = async (req, res, next) =>{
+exports.logout = async (req, res, next) => {
   try {
     const { email, token } = req.body;
-    if(!email || !token) return next(Boom.badRequest('An email or a token is required to logout !'));
+    if (!email || !token)
+      return next(
+        Boom.badRequest("An email or a token is required to logout !")
+      );
     let response = await RefreshToken.findOneAndDelete({
-      token : token,
-      userEmail : email
+      token: token,
+      userEmail: email,
     });
     return res.json(response);
   } catch (error) {
     return next(Boom.badImplementation(error.message));
   }
-}
+};
--- api/routes/v1/auth.route.js
+++ api/routes/v1/auth.route.js
@@ -1,27 +1,23 @@
-const Express = require('express'),
+const Express = require("express"),
   AuthController = require(`${process.cwd()}/api/controllers/auth.controller`),
-  { authorize, ADMIN, LOGGED_USER } = require('../../middlewares/auth.middleware');
+  {
+    authorize,
+    ADMIN,
+    LOGGED_USER,
+  } = require("../../middlewares/auth.middleware");
 
 const router = Express.Router();
 
-router
-  .route('/register')
-    .post(AuthController.register);
+router.route("/login").post(AuthController.login);
+
+router.route("/refresh-token").post(AuthController.refresh);
 
 router
-  .route('/login')
-    .post(AuthController.login);
+  .route("/check-token")
+  .get(authorize([ADMIN, LOGGED_USER]), (req, res, next) => {
+    return res.status(200).send();
+  });
 
-router
-  .route('/refresh-token')
-    .post(AuthController.refresh);
-
-router
-  .route('/check-token')
-    .get(authorize([ADMIN, LOGGED_USER]), (req, res, next) => { return res.status(200).send() });
-
-router
-  .route('/logout')
-    .post(AuthController.logout);
+router.route("/logout").post(AuthController.logout);
 
 module.exports = router;
