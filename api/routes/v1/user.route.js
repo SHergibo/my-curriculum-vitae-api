254531dd839001a2e6e9db5add5a5fd2a2948abf
SHergibo <sachau888@hotmail.com>
2022-02-01 15:06:36 +0100

    fix: Deleted findAll route in user.route.js.

--- api/routes/v1/user.route.js
+++ api/routes/v1/user.route.js
@@ -1,20 +1,21 @@
-const Express = require('express'),
-      UserController = require(`${process.cwd()}/api/controllers/user.controller`);
+const Express = require("express"),
+  UserController = require(`${process.cwd()}/api/controllers/user.controller`);
 
-const { authorize, ADMIN, LOGGED_USER } = require('../../middlewares/auth.middleware');
-
+const {
+  authorize,
+  ADMIN,
+  LOGGED_USER,
+} = require("../../middlewares/auth.middleware");
 
 const router = Express.Router();
 
-router
-    .route('/')
-        // .get(UserController.findAll)
-        //.post(UserController.add);
+router.route("/");
+//.post(UserController.add);
 
 router
-    .route('/:userId')
-        .get(authorize([ADMIN, LOGGED_USER]), UserController.findOne)
-        .patch(authorize([ADMIN, LOGGED_USER]), UserController.update)
-        .delete(authorize([ADMIN, LOGGED_USER]), UserController.remove);
+  .route("/:userId")
+  .get(authorize([ADMIN, LOGGED_USER]), UserController.findOne)
+  .patch(authorize([ADMIN, LOGGED_USER]), UserController.update)
+  .delete(authorize([ADMIN, LOGGED_USER]), UserController.remove);
 
 module.exports = router;
