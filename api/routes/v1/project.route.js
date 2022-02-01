15ab8742e424e67f8070b4accbbadaffa35e956d
SHergibo <sachau888@hotmail.com>
2022-02-01 15:05:38 +0100

    fix: Updated /project-list route name to /projects-list in project.route.js.

--- api/routes/v1/project.route.js
+++ api/routes/v1/project.route.js
@@ -1,30 +1,25 @@
-const Express = require('express'),
+const Express = require("express"),
   ProjectController = require(`${process.cwd()}/api/controllers/project.controller`);
 
-const { authorize, ADMIN, LOGGED_USER } = require('../../middlewares/auth.middleware');
-
+const {
+  authorize,
+  ADMIN,
+  LOGGED_USER,
+} = require("../../middlewares/auth.middleware");
 
 const router = Express.Router();
 
-router
-  .route('/')
-    .post(authorize([ADMIN, LOGGED_USER]), ProjectController.add);
+router.route("/").post(authorize([ADMIN, LOGGED_USER]), ProjectController.add);
 
 router
-  .route('/:projectId')
-    .patch(authorize([ADMIN, LOGGED_USER]), ProjectController.update)
-    .delete(authorize([ADMIN, LOGGED_USER]), ProjectController.remove);
+  .route("/:projectId")
+  .patch(authorize([ADMIN, LOGGED_USER]), ProjectController.update)
+  .delete(authorize([ADMIN, LOGGED_USER]), ProjectController.remove);
 
-router
-  .route('/project-list')
-    .get(ProjectController.findAll);
+router.route("/projects-list").get(ProjectController.findAll);
 
-router
-  .route('/image/:imgName')
-    .get(ProjectController.findImg);
+router.route("/image/:imgName").get(ProjectController.findImg);
 
-router
-  .route('/pagination')
-    .get(ProjectController.findPaginate);
+router.route("/pagination").get(ProjectController.findPaginate);
 
 module.exports = router;
