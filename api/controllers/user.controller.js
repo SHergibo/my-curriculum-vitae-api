b60a8eeb665dad76ef6e9f8b72e9b8164e9d47ab
SHergibo <sachau888@hotmail.com>
2022-02-01 15:04:18 +0100

    fix: Deleted useless comment in udpate use controller.

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,58 +1,57 @@
-const User = require('./../models/user.model'),
-      Boom = require('@hapi/boom'),
-      Bcrypt = require('bcrypt'),
-      { env } = require('../../config/environment.config');
+const User = require("./../models/user.model"),
+  Boom = require("@hapi/boom"),
+  Bcrypt = require("bcrypt"),
+  { env } = require("../../config/environment.config");
 
 /**
-* Post one user
-*/
-exports.add = async (req, res, next) =>{
-    try{
-        const user = new User(req.body);
-        await user.save();
-        return res.json(user.transform());
-    }catch(error){
-        next(User.checkDuplicateEmail(error));
-    }
+ * Post one user
+ */
+exports.add = async (req, res, next) => {
+  try {
+    const user = new User(req.body);
+    await user.save();
+    return res.json(user.transform());
+  } catch (error) {
+    next(User.checkDuplicateEmail(error));
+  }
 };
 
 /**
-* GET one user
-*/
-exports.findOne = async (req, res, next) =>{
-    try {
-        const user = await User.findById(req.params.userId);
-        return res.json(user.transform());
-    } catch (error) {
-        next(Boom.badImplementation(error.message));        
-    }
+ * GET one user
+ */
+exports.findOne = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.userId);
+    return res.json(user.transform());
+  } catch (error) {
+    next(Boom.badImplementation(error.message));
+  }
 };
 
 /**
-* PATCH user
-*/
+ * PATCH user
+ */
 exports.update = async (req, res, next) => {
-    try {
-        // if(req.body.password){
-        //     let salt = env === 'staging' ? 1 : 10;
-        //     let hash = await Bcrypt.hash(req.body.password, salt);
-        //     req.body.password = hash;
-        // }
-        const user = await User.findByIdAndUpdate(req.params.userId,  req.body, {override : true, upsert : true, new : true});
-        return res.json(user.transform());
-    } catch (error) {
-        next(User.checkDuplicateEmail(err)); 
-    }
+  try {
+    const user = await User.findByIdAndUpdate(req.params.userId, req.body, {
+      override: true,
+      upsert: true,
+      new: true,
+    });
+    return res.json(user.transform());
+  } catch (error) {
+    next(User.checkDuplicateEmail(err));
+  }
 };
 
 /**
-* DELETE user
-*/
+ * DELETE user
+ */
 exports.remove = async (req, res, next) => {
-    try {
-        const user = await User.findByIdAndDelete(req.params.userId);
-        return res.json(user.transform());
-    } catch (error) {
-        next(Boom.badImplementation(error.message));        
-    }
+  try {
+    const user = await User.findByIdAndDelete(req.params.userId);
+    return res.json(user.transform());
+  } catch (error) {
+    next(Boom.badImplementation(error.message));
+  }
 };
