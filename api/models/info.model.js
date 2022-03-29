d80c2a56f7ea6f1883a63fc02e1a67867df74c4e
SHergibo <sachau888@hotmail.com>
2022-03-29 16:50:19 +0200

    feat: Added professionTitles field in info model.

--- api/models/info.model.js
+++ api/models/info.model.js
@@ -1,79 +1,135 @@
-const Mongoose = require('mongoose');
+const Mongoose = require("mongoose"),
+  DomPurify = require("isomorphic-dompurify");
 
 let Schema = Mongoose.Schema;
 
-let schema = new Schema({
-    firstname : {
-        type : String,
-        required : true,
-        trim : true
+let fontAwesomeIconSchema = new Schema(
+  {
+    label: {
+      type: String,
+      required: true,
+      trim: true,
     },
-    lastname : {
-        type : String,
-        required : true,
-        trim : true
+    value: {
+      type: String,
+      required: true,
+      trim: true,
     },
-    phone : {
-        type : String,
-        required : true,
-        trim : true
+    prefix: {
+      type: String,
+      required: true,
+      trim: true,
     },
-    email : {
-        type : String,
-        required : true,
-        trim : true
+  },
+  {
+    _id: false,
+  }
+);
+
+let profTitleSchema = new Schema({
+  nameProfessionTitle: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  fontAwesomeIcon: {
+    type: fontAwesomeIconSchema,
+  },
+  svgIconProfTitle: {
+    type: String,
+    set: function (val) {
+      return DomPurify.sanitize(val);
     },
-    address : {
-        street : {
-            type : String,
-            required : true,
-            trim : true
-        },
-        number : {
-            type : String,
-            required : true,
-            trim : true
-        },
-        zip : {
-            type : String,
-            required : true,
-            trim : true
-        },
-        city : {
-            type : String,
-            required : true,
-            trim : true
-        }
-    },
-    birthdate : {
-        type : Date,
-        required : true,
-        trim : true
-    },
-    licence : {
-        type : String,
-        required : true,
-        trim : true
-    },
-    description : {
-        type : String,
-        required : true,
-        trim : true
-    },
-    userId : {
-        type : Schema.Types.ObjectId,
-        ref : 'User',
-        required : true,
-    },
+  },
 });
 
-schema.methods.transformInfo = function() {
-    const fields = ['_id', 'firstname', 'lastname', 'phone', 'email', 'address', 'birthdate', "licence", "description"];
-    const object = {};
-    fields.forEach((field)=>{
-        object[field] = this[field];
-    });
-    return object;
+let schema = new Schema({
+  firstname: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  lastname: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  address: {
+    street: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    number: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    zip: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    city: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+  },
+  birthdate: {
+    type: Date,
+    required: true,
+    trim: true,
+  },
+  licence: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  professionTitles: {
+    type: [profTitleSchema],
+  },
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+});
+
+schema.methods.transformInfo = function () {
+  const fields = [
+    "_id",
+    "firstname",
+    "lastname",
+    "phone",
+    "email",
+    "address",
+    "birthdate",
+    "licence",
+    "description",
+    "professionTitles",
+    "userId",
+  ];
+  const object = {};
+  fields.forEach((field) => {
+    object[field] = this[field];
+  });
+  return object;
 };
 
-module.exports = Mongoose.model('Info', schema);
+module.exports = Mongoose.model("Info", schema);
