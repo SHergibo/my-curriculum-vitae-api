0328341d899f8f921dd554e0414badb97fa47e47
SHergibo <sachau888@hotmail.com>
2022-04-05 14:20:07 +0200

    fix: Changed status from 200 to 204 in /check-token route in auth.route.js.

--- api/routes/v1/auth.route.js
+++ api/routes/v1/auth.route.js
@@ -15,7 +15,7 @@ router.route("/refresh-token").post(AuthController.refresh);
 router
   .route("/check-token")
   .get(authorize([ADMIN, LOGGED_USER]), (req, res, next) => {
-    return res.status(200).send();
+    return res.status(204).send();
   });
 
 router.route("/logout").post(AuthController.logout);
