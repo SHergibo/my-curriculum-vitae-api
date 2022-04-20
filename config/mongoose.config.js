313772e4cebf7975f49887b77d943f91c5c76c79
SHergibo <sachau888@hotmail.com>
2022-04-20 15:15:20 +0200

    fix: Deleted use of nodemailer in mongoose.config.js.

--- config/mongoose.config.js
+++ config/mongoose.config.js
@@ -1,35 +1,33 @@
-const Mongoose = require('mongoose');
-const {loggerError, loggerInfo} = require('./logger.config');
-const NodeMailer = require('./../api/helpers/nodemailer.helper');
+const Mongoose = require("mongoose");
+const { loggerError, loggerInfo } = require("./logger.config");
 
-const { mongo, env, environments } = require('./environment.config');
+const { mongo, env, environments } = require("./environment.config");
 
 Mongoose.Promise = global.Promise;
 
-Mongoose.set('returnOriginal', false);
+Mongoose.set("returnOriginal", false);
 
-Mongoose.connection.on('error', (err) =>{
-    if(env.toUpperCase() === environments.PRODUCTION){
-        loggerError.error(`MongoDB connection error: ${err}`);
-        NodeMailer.send(error, 'Une erreur est survenue lors de la connection à MongoDB !');
-    }else{
-        console.log(err)
-    }
-    process.exit(-1);
-})
+Mongoose.connection.on("error", (err) => {
+  if (env.toUpperCase() === environments.PRODUCTION) {
+    loggerError.error(`MongoDB connection error: ${err}`);
+  } else {
+    console.log(err);
+  }
+  process.exit(-1);
+});
 
-if(env.toUpperCase() === environments.DEVELOPMENT){
-    Mongoose.set('debug', true);
+if (env.toUpperCase() === environments.DEVELOPMENT) {
+  Mongoose.set("debug", true);
 }
 
 exports.connect = () => {
-    Mongoose.connect(mongo.uri, {
-        keepAlive : 1,
-    });
-    if(env.toUpperCase() === environments.PRODUCTION){
-        loggerInfo.info('MongoDB server is now running on port 27017');
-    }else{
-        console.log('MongoDB server is now running on port 27017');
-    }
-    return Mongoose.connection;
-}
+  Mongoose.connect(mongo.uri, {
+    keepAlive: 1,
+  });
+  if (env.toUpperCase() === environments.PRODUCTION) {
+    loggerInfo.info("MongoDB server is now running on port 27017");
+  } else {
+    console.log("MongoDB server is now running on port 27017");
+  }
+  return Mongoose.connection;
+};
