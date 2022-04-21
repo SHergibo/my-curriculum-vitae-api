bc1c0c9b772f840be13188902c2a48355f0bc851
SHergibo <sachau888@hotmail.com>
2022-04-21 15:26:31 +0200

    feat: Renamed token-auth model to emailAuthToken and updated model and generate statics function.

---
+++ api/models/emailAuthToken.model.js
@@ -0,0 +1,60 @@
+const Mongoose = require("mongoose"),
+  Crypto = require("crypto"),
+  Moment = require("moment-timezone"),
+  {
+    SendGridAPIKey,
+    SendGridFrom,
+    UrlEmail,
+  } = require("../../config/environment.config"),
+  sgMail = require("@sendgrid/mail");
+
+sgMail.setApiKey(SendGridAPIKey);
+
+let Schema = Mongoose.Schema;
+
+let schema = new Schema({
+  token: {
+    type: String,
+    required: true,
+    index: true,
+  },
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  userEmail: {
+    type: String,
+    ref: "User",
+    required: true,
+  },
+  expires: {
+    type: Date,
+    required: true,
+  },
+});
+
+schema.statics.generate = async function (user) {
+  const tokenObject = new emailAuthToken();
+  tokenObject.token = `${user._id}.${Crypto.randomBytes(40).toString("hex")}`;
+  tokenObject.userId = user._id;
+  tokenObject.userEmail = user.email;
+  tokenObject.expires = Moment().add(5, "minutes").toDate();
+  await tokenObject.save();
+
+  const msg = {
+    to: user[0].email,
+    from: SendGridFrom,
+    subject: "Authentification de votre compte !",
+    html: `
+      <h2>Authentification de votre compte</h2>
+      <p>Cliquez sur le lien ci-dessous pour authentifier votre compte.<p>
+      <a href="${UrlEmail}/email-auth/${newToken.token}">Auhthentifier votre compte</a>
+      `,
+  };
+  await sgMail.send(msg);
+};
+
+const emailAuthToken = Mongoose.model("EmailAuthToken", schema);
+
+module.exports = emailAuthToken;
