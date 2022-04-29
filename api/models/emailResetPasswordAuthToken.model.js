fd046e391a6cdfdaf97e23bc28d59dff38e0339d
SHergibo <sachau888@hotmail.com>
2022-04-29 15:05:36 +0200

    feat: Added warning message in reset passord mail in generate statics function in emailResetPasswordAuthToken model.

--- api/models/emailResetPasswordAuthToken.model.js
+++ api/models/emailResetPasswordAuthToken.model.js
@@ -49,6 +49,7 @@ schema.statics.generate = async function (user) {
     html: `
       <h2>Requête changement de mot de passe !</h2>
       <p>Cliquez sur le lien ci-dessous pour pouvoir changer votre mot de passe.<p>
+      <p>Si vous n'avez pas fait cette demande de changement de mot de passe, ne partagez pas et ne cliquez pas sur lien ci-dessous !</p>
       <a href="${UrlEmail}/reset-password/${tokenObject.token}">Changer votre mot de passe !</a>
       `,
   };
