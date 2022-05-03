2320c31e40af7d6aa047621b59dcb17f9f2849b2
SHergibo <sachau888@hotmail.com>
2022-05-03 15:26:30 +0200

    feat: Added error guarding and delete other user account with ghost role in add user controller and created checkUserExist user controller.

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -7,6 +7,21 @@ const User = require("./../models/user.model"),
  */
 exports.add = async (req, res, next) => {
   try {
+    const findAdminUser = await User.findOne({ role: "admin" });
+    if (findAdminUser) {
+      return next(
+        Boom.unauthorized(
+          "Il existe déjà un compte authentifié, vous ne pouvez pas créer un second compte !"
+        )
+      );
+    }
+
+    const findGhostUser = await User.findOne({ role: "ghost" });
+    if (findGhostUser) {
+      await User.findByIdAndDelete(findGhostUser._id);
+      await EmailAuthToken.findOneAndDelete({ userId: findGhostUser._id });
+    }
+
     const user = new User(req.body);
     await user.save();
     await EmailAuthToken.generate(user);
@@ -23,6 +38,21 @@ exports.findOne = async (req, res, next) => {
   try {
     const user = await User.findById(req.params.userId);
     return res.json(user.transform());
+  } catch (error) {
+    next(Boom.badImplementation(error.message));
+  }
+};
+
+/**
+ * GET Check if a user exist
+ */
+exports.checkUserExist = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ role: "admin" });
+    let userExist = false;
+    if (user) userExist = true;
+
+    return res.json({ userExist });
   } catch (error) {
     next(Boom.badImplementation(error.message));
   }
