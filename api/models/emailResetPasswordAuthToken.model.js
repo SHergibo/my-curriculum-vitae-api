8f5ba348111391b1ea47343eb09567022113da9f
SHergibo <sachau888@hotmail.com>
2022-05-05 16:25:35 +0200

    feat: Added templateId and dynamic template data for msg sended via sendGrid in generate statics function in emailResetPasswordAuthToken model.

--- api/models/emailResetPasswordAuthToken.model.js
+++ api/models/emailResetPasswordAuthToken.model.js
@@ -45,16 +45,37 @@ schema.statics.generate = async function (user) {
   const msg = {
     to: user.email,
     from: SendGridFrom,
-    subject: "Requête changement de mot de passe !",
-    html: `
-      <h2>Requête changement de mot de passe !</h2>
-      <p>Cliquez sur le lien ci-dessous pour pouvoir changer votre mot de passe.<p>
-      <p>Si vous n'avez pas fait cette demande de changement de mot de passe, ne partagez pas et ne cliquez pas sur lien ci-dessous !</p>
-      <a href="${UrlEmail}/reset-password/${tokenObject.token}">Changer votre mot de passe !</a>
-      `,
+    templateId: "d-9b0d1c9ef5d04f4ba4a2cb5b8e71b913",
+    dynamic_template_data: {
+      subject: "Requête changement de mot de passe !",
+      title: "Requête changement de mot de passe ! - votre CV en ligne",
+      text: `<p>Cliquez sur le lien ci-dessous pour pouvoir changer votre mot de passe.<p>
+             <p>Si vous n'avez pas fait cette demande de changement de mot de passe, ne partagez pas et ne cliquez pas sur lien ci-dessous !</p>`,
+      url: `${UrlEmail}/reset-password/${tokenObject.token}`,
+      urlText: "Changer votre mot de passe !",
+    },
+    // html: `
+    //   <div class="title">
+    //     <h2>Requête changement de mot de passe ! - votre CV en ligne</h2>
+    //   </div>
+    //   <section>
+    //     <p>Cliquez sur le lien ci-dessous pour pouvoir changer votre mot de passe.<p>
+    //     <p>Si vous n'avez pas fait cette demande de changement de mot de passe, ne partagez pas et ne cliquez pas sur lien ci-dessous !</p>
+    //     <a class="linkTo" href="${UrlEmail}/reset-password/${tokenObject.token}">Changer votre mot de passe !</a>
+    //   </section>
+    //   `,
   };
   await sgMail.send(msg);
 };
+
+// {/* <div class="title">
+//   <h2>Requête changement de mot de passe !</h2>
+// </div>
+// <section>
+//     <p>Cliquez sur le lien ci-dessous pour pouvoir changer votre mot de passe.<p>
+//     <p>Si vous n'avez pas fait cette demande de changement de mot de passe, ne partagez pas et ne cliquez pas sur lien ci-dessous !</p>
+//   <a class="linkTo" href={{url}}>Changer votre mot de passe !</a>
+// </section> */}
 
 const emailResetPasswordAuthToken = Mongoose.model(
   "EmailResetPasswordAuthToken",
