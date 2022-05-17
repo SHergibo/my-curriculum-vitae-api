af5f5d478098618d84a78f4e85d318e781a1d0f5
SHergibo <sachau888@hotmail.com>
2022-05-17 14:52:48 +0200

    feat: Added /updatePassword and updateEmail route in user route.

--- api/routes/v1/user.route.js
+++ api/routes/v1/user.route.js
@@ -12,9 +12,16 @@ const router = Express.Router();
 router.route("/").post(UserController.add).get(UserController.checkUserExist);
 
 router
+  .route("/updatePassword/:userId")
+  .patch(authorize([ADMIN, LOGGED_USER]), UserController.updatePassword);
+
+router
+  .route("/updateEmail/:userId")
+  .patch(authorize([ADMIN, LOGGED_USER]), UserController.updateEmail);
+
+router
   .route("/:userId")
   .get(authorize([ADMIN, LOGGED_USER]), UserController.findOne)
-  .patch(authorize([ADMIN, LOGGED_USER]), UserController.update)
   .delete(authorize([ADMIN, LOGGED_USER]), UserController.remove);
 
 module.exports = router;
