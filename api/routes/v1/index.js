f487cc727fe439b7ab3daf17819f40cac5d52a73
SHergibo <sachau888@hotmail.com>
2022-05-17 14:55:34 +0200

    feat: Added /change-email-auth route in index in /routes/v1 folder.

--- api/routes/v1/index.js
+++ api/routes/v1/index.js
@@ -6,14 +6,16 @@ const Express = require("express"),
   SkillRoutes = require("./skill.route"),
   ProjectRoutes = require("./project.route"),
   AuthRoutes = require("./auth.route"),
-  EmailAuthTokenRoutes = require("./emailAuthToken.route");
-EmailResetPasswordAuthToken = require("./emailResetPasswordAuthToken.route");
+  EmailAuthTokenRoutes = require("./emailAuthToken.route"),
+  EmailResetPasswordAuthToken = require("./emailResetPasswordAuthToken.route"),
+  EmailChangeEmailAuthToken = require("./emailChangeEmailAuthToken.route");
 
 const router = Express.Router();
 
 router.use("/auth", AuthRoutes);
 router.use("/email-auth", EmailAuthTokenRoutes);
 router.use("/email-request-reset-password", EmailResetPasswordAuthToken);
+router.use("/change-email-auth", EmailChangeEmailAuthToken);
 router.use("/users", UserRoutes);
 router.use("/mails", MailRoutes);
 router.use("/infos", InfoRoutes);
